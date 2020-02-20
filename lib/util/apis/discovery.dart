8e55b1b2056da5aeeab376af9ea2d31f8c106153
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-02-20 21:25:32 +0530

    minor array check and cards added

--- lib/util/apis/discovery.dart
+++ lib/util/apis/discovery.dart
@@ -6,20 +6,22 @@ import '../data/Device.dart';
 
 Future<List<Device>> getDiscoveredList([Map<String, String> data = const {}]) {
   return helper.makeGetRequest(urls.DISCOVERY_ENDPOINT, data).then((response) {
-    if( response.statusCode != 200 ){
+    if (response.statusCode != 200) {
       // Server responded with error code
       throw Exception();
     }
-    
+
     var parsedJson = jsonDecode(response.body);
 
     List<Device> devices = new List<Device>();
-    for (var item in parsedJson["deviceArray"]) {
-      devices.add(new Device(item["id"], item["name"]));
+    if (parsedJson["deviceArray"] is List) {
+      for (var item in parsedJson["deviceArray"]) {
+        devices.add(new Device(item["id"], item["name"]));
+      }
     }
+
     return devices;
-  }).catchError((onError){
-    // Connection failed
+  }).catchError((onError) {
     throw Exception();
   });
 }
